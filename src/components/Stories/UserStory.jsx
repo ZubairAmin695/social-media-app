e6f75f5378314de2d460317149aa03634e71d223
ZubairAmin695 <zubairamin695@gmail.com>
2023-10-09 16:59:15 +0500

    responsiveness

--- src/components/Stories/UserStory.jsx
+++ src/components/Stories/UserStory.jsx
@@ -4,16 +4,20 @@ import "./story.css"
 
 
 import CurrenUserData from "../../FakeApis/CurrentUserData"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdd } from '@fortawesome/free-solid-svg-icons'
 const UserStory = () => {
   return (
-    <div className="story">
+    <div className="story userStory">
         <div className="user">
-            <img src={CurrenUserData.ProfileImage} alt=""/>
+            <img src={CurrenUserData.map(user=>(user.ProfieImage))} alt=""/>
         </div>
-        <img src={CurrenUserData.ProfileImage} alt=""/>
-      <h5>
-      
-      </h5>
+        <img src={CurrenUserData.map(user=>(user.CoverPhoto))} alt=""/>
+      <label htmlFor='storyFiles'>
+        <FontAwesomeIcon icon={faAdd}/>
+        <input type='file' id="storyFiles"/>
+      </label>
+      <h5>Add Story</h5>
     </div>
   )
 }
